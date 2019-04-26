50bc685a1dae552eb05dfba0bfbff3fc6e0baf4c
CountryNerd <reisenauer.james@gmail.com>
2019-04-26 15:22:03 -0700

    Refactored to use a watcher to switch contant type

--- main.js
+++ main.js
@@ -17,21 +17,70 @@ var app = new Vue ({
             { 
             status: "available", 
             name: "Christian",
-            emailAddress: "Christian@edu",
-            phoneNumber: "509..."
+            emailAddress: "Christian@yahoo.com",
+            phoneNumber: "509-555-1234", 
+            selectedInfo: "Christian@ahoo.com"
             },
             { 
+            status: "available", 
+            name: "Rich",
+            emailAddress: "rich@tripod.com",
+            phoneNumber: "509-555-1234",
+            selectedInfo: "rich@tripod.com"
+          },
+          { 
+            status: "available", 
+            name: "Scott",
+            emailAddress: "scott@mailinator.com",
+            phoneNumber: "509-555-1234",
+            selectedInfo: "scott@mailinator.com"
+          },
+          { 
+            status: "available", 
+            name: "Danny",
+            emailAddress: "danny@hotmail.com",
+            phoneNumber: "509-555-1234",
+            selectedInfo: "danny@hotmail.com"
+          },
+          { 
             status: "away", 
-            name: "Joe",
-            emailAddress: "Joe@edu",
-            phoneNumber: "509"
+            name: "Taka",
+            emailAddress: "taka@myspace.com",
+            phoneNumber: "509-555-1234",
+            selectedInfo: "taka@myspace.com"
           },
           { 
             status: "standby", 
-            name: "Lesli",
-            emailAddress: "lesli@edu",
-            phoneNumber: "509"
+            name: "Tim",
+            emailAddress: "tim@netscape.com",
+            phoneNumber: "509-555-1234",
+            selectedInfo: "tim@netscape.com"
+          },
+          { 
+            status: "available", 
+            name: "Patrick",
+            emailAddress: "patrick@live.com",
+            phoneNumber: "509-555-1234",
+            selectedInfo: "patrick@live.com"
+          },
+          { 
+            status: "standby", 
+            name: "Jacques",
+            emailAddress: "jacques@aol.com",
+            phoneNumber: "509-555-1234",
+            selectedInfo: "jacques@aol.com"
           },
         ]
+    },
+    watch: {
+        contactTypeSelected: function() {
+            this.people.forEach(person => {
+                if(this.contactTypeSelected == "email"){
+                    person.selectedInfo = person.emailAddress;
+                } else{
+                    person.selectedInfo = person.phoneNumber;
+                } 
+            });
+        }
     }
 })
